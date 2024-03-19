a8b54f13a0350684509e6428842d3df91f0bc5f1
atacan <atacandur@icloud.com>
2024-03-19 14:21:19 +0100

    diffAttributedString

--- Package.swift
+++ Package.swift
@@ -1,14 +1,20 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.9
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftDiff",
+    platforms: [.macOS(.v14), .iOS(.v16), .watchOS(.v6), .tvOS(.v13)],
     products: [
-        .library(name: "SwiftDiff", targets: ["SwiftDiff"])
+        .library(name: "SwiftDiff", targets: ["SwiftDiff"]),
+        .library(name: "SwiftDiffSwiftUI", targets: ["SwiftDiffSwiftUI"])
     ],
     targets: [
         .target(name: "SwiftDiff"),
+        .target(name: "SwiftDiffSwiftUI", dependencies: ["SwiftDiff"]),
+        .executableTarget (
+            name: "_Playground",
+            dependencies: ["SwiftDiffSwiftUI"]),
         .testTarget(name: "SwiftDiffTests", dependencies: ["SwiftDiff"]),
     ]
 )
---
+++ Sources/SwiftDiffSwiftUI/SwiftDiffSwiftUI.swift
@@ -0,0 +1,31 @@
+import SwiftDiff
+import Foundation
+import SwiftUI
+
+// function that crates a AttributeString from a diff
+func attributedString(from diffs: [Diff]) -> AttributedString {
+    var attributedString = AttributedString()
+
+    for diff in diffs {
+        switch diff {
+        case let .delete(text):
+            var attrStr = AttributedString(text)
+            attrStr.foregroundColor = .red
+            attributedString.append(attrStr)
+        case let .insert(text):
+            var attrStr = AttributedString(text)
+            attrStr.foregroundColor = .green
+            attributedString.append(attrStr)
+        case let .equal(text):
+            attributedString.append(AttributedString(text))
+        }
+    }
+
+    return attributedString
+}
+
+// AttributeString from two texts
+public func diffAttributedString(text1: String, text2: String) -> AttributedString {
+    let diffs = diff(text1: text1, text2: text2)
+    return attributedString(from: diffs)
+}
